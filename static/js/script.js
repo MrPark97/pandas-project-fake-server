1c3f4a4aef01275b09f0132d6d681cab3811909d
MrPark97 <evgeniy.pak.97@mail.ru>
2017-12-31 13:20:49 +0500

    done

--- static/js/script.js
+++ static/js/script.js
@@ -2,6 +2,8 @@ request12 = new ajaxLolRequest();
 
 
 function compileIt() {
+	document.getElementById('loading-transparent').setAttribute('style', 'display: block;');
+	document.getElementById('loading').setAttribute('style', 'height:200px; width: 200px;');
 
 	request12.open('POST', '/', true);
 	request12.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
@@ -18,7 +20,30 @@ request12.onreadystatechange = function() {
 	if (this.readyState == 4) {
 		if (this.status == 200) {
 			if(this.responseText != null) {
-				document.getElementById("compilation-output").innerHTML = this.responseText;
+				var response = JSON.parse(this.responseText);
+
+				setTimeout(function() {
+					
+					if(response.error != "") {
+						var element = document.createElement("span");
+						element.innerHTML = response.error;
+						element.setAttribute('class', 'compilation-error');
+						document.getElementById("compilation-output").innerHTML = "";
+						document.getElementById("compilation-output").append(element);
+					} else {
+						document.getElementById("compilation-output").innerHTML = response.output;
+						for(i in response.warnings) {
+							var element = document.createElement("span");
+							element.innerHTML = response.warnings[i];
+							element.setAttribute('class', 'compilation-warning');
+							document.getElementById("compilation-output").innerHTML += "<br />";
+							document.getElementById("compilation-output").append(element);
+						}
+					}
+
+					document.getElementById('loading-transparent').removeAttribute('style');
+					document.getElementById('loading').removeAttribute('style');
+				}, 1000);
 			}
 		}
 	}
